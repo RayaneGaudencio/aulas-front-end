203f8afb941e7938b58edba34ca72eca91bf88db
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-03-08 16:32:38 -0300

    projeto MUSIC

---
+++ MUSIC/js/index.js
@@ -0,0 +1,7 @@
+function tornaTextoInvisivel() {
+    let tituloVideo = document.querySelector("#titulo-video");
+    tituloVideo.style.display = "none";
+
+    let video2 = document.querySelector("#video2");
+    video2.play();
+}
